d60d2b77e1942af096c23f6d0f88bd44d3662ac2
Azutch <azu0032ma49@gmail.com>
2026-08-22 20:15:54 +0900

    api.js を更新

--- functions/api.js
+++ functions/api.js
@@ -1,32 +1,17 @@
 const KAGI = "kai-list";
 
-const SAISHO_NO_DATA = [
-  {
-    eventMei: "バンドイベント",
-    hizuke: "2026-09-12",
-    hitsuyouNinzuu: 5,
-    shutsuensha: ["山田", "佐藤"]
-  },
-  {
-    eventMei: "DJイベント",
-    hizuke: "2026-10-03",
-    hitsuyouNinzuu: 4,
-    shutsuensha: ["田中", "鈴木", "高橋", "伊藤"]
-  }
-];
-
 export async function onRequestGet(context) {
   const hairetteta = await context.env.OFFER_KV.get(KAGI);
 
   let data;
   if (hairetteta === null) {
-    data = SAISHO_NO_DATA;
+    data = [];
   } else {
     data = JSON.parse(hairetteta);
   }
 
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" }
+    headers: { "Content-Type": "application/json; charset=utf-8" }
   });
 }
 
@@ -35,6 +20,6 @@ export async function onRequestPost(context) {
   await context.env.OFFER_KV.put(KAGI, okuraretaMono);
 
   return new Response(JSON.stringify({ kekka: "ok" }), {
-    headers: { "Content-Type": "application/json; charset=utf-8"
-
+    headers: { "Content-Type": "application/json; charset=utf-8" }
+  });
 }
