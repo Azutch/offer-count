bf5ffffd599b8bf570bb3b59f9e449ed03c62e6b
Azutch <azu0032ma49@gmail.com>
2026-08-22 19:50:49 +0900

    api.js を更新

--- functions/api.js
+++ functions/api.js
@@ -35,6 +35,6 @@ export async function onRequestPost(context) {
   await context.env.OFFER_KV.put(KAGI, okuraretaMono);
 
   return new Response(JSON.stringify({ kekka: "ok" }), {
-    headers: { "Content-Type": "application/json" }
-  });
+    headers: { "Content-Type": "application/json; charset=utf-8"
+
 }
