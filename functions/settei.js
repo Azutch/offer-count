ffd261beb5864e2c16284a056ed94cf754472c81
Azutch <azu0032ma49@gmail.com>
2026-08-24 22:10:40 +0900

    settei.js を作成

---
+++ functions/settei.js
@@ -0,0 +1,30 @@
+const KAGI = "settei";
+
+// まだ何も入っていないときの、最初の中身
+const SAISHO_NO_DATA = {
+  linkList: []
+};
+
+export async function onRequestGet(context) {
+  const hairetteta = await context.env.OFFER_KV.get(KAGI);
+
+  let data;
+  if (hairetteta === null) {
+    data = SAISHO_NO_DATA;
+  } else {
+    data = JSON.parse(hairetteta);
+  }
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json; charset=utf-8" }
+  });
+}
+
+export async function onRequestPost(context) {
+  const okuraretaMono = await context.request.text();
+  await context.env.OFFER_KV.put(KAGI, okuraretaMono);
+
+  return new Response(JSON.stringify({ kekka: "ok" }), {
+    headers: { "Content-Type": "application/json; charset=utf-8" }
+  });
+}
